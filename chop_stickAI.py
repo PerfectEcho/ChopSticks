350291fef96ed7f7239e22963c047e9b65a964c7
Brandon Abbott <barabbit907@gmail.com>
2019-02-08 21:32:38 -0900

    start

---
+++ chop_stickAI.py
@@ -0,0 +1,13 @@
+#Chop Sticks
+#Brandon Abbott
+
+Class Game:
+    def __init__(self):
+        self.player_one_left =  1
+        self.player_one_right = 1
+        self.player_two_left = 1
+        self.player_two_right= 1
+    #    self.computer_one_on = False
+    #    self.computer_two_on = False
+
+    def tap(self)
