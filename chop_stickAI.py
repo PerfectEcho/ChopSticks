e94dac1b9173f8402cd156b974537fdaafb1477b
Brandon Abbott <barabbit907@gmail.com>
2019-02-09 03:54:59 -0900

    basic finger counting fucntion

--- chop_stickAI.py
+++ chop_stickAI.py
@@ -1,13 +1,36 @@
 #Chop Sticks
 #Brandon Abbott
+import sys
 
-Class Game:
+class Chop_Sticks:
     def __init__(self):
         self.player_one_left =  1
         self.player_one_right = 1
         self.player_two_left = 1
         self.player_two_right= 1
-    #    self.computer_one_on = False
-    #    self.computer_two_on = False
+        self.computer_on = True
+        self.player_one_turn = True
+        self.not_at_end = True
 
-    def tap(self)
+        #self.computer_two = False
+
+    def tap(self, to_left_hand, from_left_hand):
+        if self.player_one_turn:
+            atck_value = self.player_one_left if from_left_hand else self.player_one_right
+            if to_left_hand:
+                self.player_two_left+=atck_value
+            else:
+                self.player_two_right+=atck_value
+
+game = Chop_Sticks()
+while game.not_at_end:
+    hand = input()
+    if hand == 1:
+       game.tap(True,True)
+       print(game.player_two_left)
+       print("left was clicked")
+    else:
+        game.tap(False, False)
+        print(game.player_two_right)
+        print("right was clicked")
+
