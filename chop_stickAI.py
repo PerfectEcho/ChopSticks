2d164fcb060305c062f3370b38a4cc0aef901d07
Brandon Abbott <barabbit907@gmail.com>
2019-02-09 22:43:24 -0900

    FIXED IT: In Game state

--- chop_stickAI.py
+++ chop_stickAI.py
@@ -8,29 +8,50 @@ class Chop_Sticks:
         self.player_one_right = 1
         self.player_two_left = 1
         self.player_two_right= 1
-        self.computer_on = True
-        self.player_one_turn = True
         self.not_at_end = True
+        self.playerturn = True
 
-        #self.computer_two = False
-
-    def tap(self, to_left_hand, from_left_hand):
-        if self.player_one_turn:
-            atck_value = self.player_one_left if from_left_hand else self.player_one_right
+    def tap(self, to_left_hand, from_left_hand,playerturn):
+        if playerturn:
             if to_left_hand:
+                atck_value = self.player_one_left if from_left_hand else self.player_one_right
                 self.player_two_left+=atck_value
+                if self.player_two_left > 5:
+                    self.player_two_left = self.player_two_left - 5
+                
+                print("was player 1 turn")
+                print(self.player_two_left)
+                self.playerturn = False
             else:
+                atck_value = self.player_two_left if from_left_hand else self.player_two_right
                 self.player_two_right+=atck_value
+                if self.player_two_right > 5:
+                    self.player_two_right = self.player_two_right - 5
+                self.playerturn = False
+                print("was player 1 turn")
+                print(self.player_two_right)
+        else:
+            if to_left_hand:
+                atck_value = self.player_two_left if from_left_hand else self.player_two_right
+                self.player_one_left+=atck_value
+                self.playerturn = True
+                print("was player 2 turn")
+                print(self.player_one_left)
+            else:
+                atck_value = self.player_two_left if from_left_hand else self.player_two_right
+                self.player_one_right+=atck_value
+                self.playerturn = True
+                print("was player 2 turn")
+                print(self.player_one_right)
 
 game = Chop_Sticks()
 while game.not_at_end:
+    print("Left hand = 1 : Right hand = 0 ")
     hand = input()
     if hand == 1:
-       game.tap(True,True)
-       print(game.player_two_left)
-       print("left was clicked")
+        game.tap(True,True,game.playerturn)
+    elif hand == 9:
+        game.tap(False,False,game.playerturn)
+        
     else:
-        game.tap(False, False)
-        print(game.player_two_right)
-        print("right was clicked")
-
+        print("try again")
