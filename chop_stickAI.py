8b00318ea7dfaf4e398ab7e4f7b1f370f3385631
Brandon Abbott <barabbit907@gmail.com>
2019-02-12 15:59:27 -0900

    Its a mess but working-ish

--- chop_stickAI.py
+++ chop_stickAI.py
@@ -2,26 +2,39 @@
 #Brandon Abbott
 import sys
 
+RECURSION_DEPTH = 9
+
 class Chop_Sticks:
     def __init__(self):
         self.player_one_left =  1
         self.player_one_right = 1
         self.player_two_left = 1
         self.player_two_right= 1
-        self.not_at_end = True
         self.playerturn = True
 
+    def get_state(self):
+        return {'p1l': self.player_one_left, 'p1r': self.player_one_right, 'p2l': self.player_two_left, 'p2r':self.player_two_right, 'playerturn': self.playerturn}
+    
+    def set_state(self, state):
+        self.player_one_left = state['p1l']
+        self.player_one_right = state['p1r']
+        self.player_two_left = state['p2l']
+        self.player_two_right = state['p2r']
+        self.playerturn = state ['playerturn']
+    
     def player_defeat(self):
         if self.player_one_left == self.player_one_right == 0:
-            print('Player Defeated')
-            self.not_at_end = False
-            return 0
-        elif self.player_two_left == self.player_two_right == 0:
-            print('Player Defeated')
-            self.not_at_end = False
-            return 0
+            print('Player 1 Defeated')
+            return True
         else:
-            self.not_at_end =True
+            print('Player 2 Defeated')
+            return False
+
+    def ended(self):
+        if (self.player_one_left == self.player_one_right == 0) or (self.player_two_left == self.player_two_right == 0):
+            return True
+        else:
+            return False
 
     def swap(self):
         if self.playerturn:
@@ -32,7 +45,6 @@ class Chop_Sticks:
             print(self.player_two_left,"        ",self.player_two_right)
             print(self.player_one_left,"        ",self.player_one_right)
             print("\n")
-            self.playerturn = True
         else:
             temp = self.player_two_left
             self.player_two_left = self.player_two_right
@@ -41,7 +53,6 @@ class Chop_Sticks:
             print(self.player_two_left,"        ",self.player_two_right)
             print(self.player_one_left,"        ",self.player_one_right)
             print("\n")
-            self.playerturn = False
 
     def split(self):
         total_one = (self.player_one_left + self.player_one_right)
@@ -49,96 +60,182 @@ class Chop_Sticks:
         if self.playerturn:
             if total_one % 2 != 0 :
                 print("Can't split")
-                self.playerturn = True
+                return False
             else:
                 if self.player_one_left == self.player_one_right:
                     print("Can't split")
-                    self.playerturn = True
+                    return False
                 else:
                     self.player_one_left = total_one / 2
                     self.player_one_right = total_one / 2
-                    self.playerturn = True
         else:
             if total_two % 2 != 0 :
                 print("Can't split")
-                self.playerturn = False
+                return False
             else:
                 if self.player_two_left == self.player_two_right:
                     print("Can't split")
-                    self.playerturn = False
+                    return False
                 else:
                     self.player_two_left = total_two / 2
                     self.player_two_right = total_two / 2
-                    self.playerturn = False
+        self.playerturn = not self.playerturn
+        return True
 
     def tap(self, to_left_hand):
         if self.playerturn:
             if to_left_hand:
                 #Player 1's left hand taps Player 2's left hand
-                if self.player_two_left == 0: 
+                if self.player_two_left == 0 or self.player_one_left == 0: 
                     print('That hand is out try again')
-                    self.playerturn = True
+                    return False
                 else:
                     self.player_two_left += self.player_one_left
                     if self.player_two_left >= 5:
-                        self.player_two_left = self.player_two_left % 5
-                        self.playerturn = False
-                    else: 
-                        self.playerturn = False
+                        self.player_two_left = 0
             else:
-                #Player 1's right hand taps Player 2's left hand
-                if self.player_two_right == 0: 
+                #Player 1's right hand taps Player 2's right hand
+                if self.player_two_right == 0 or self.player_one_right == 0: 
                     print('That hand is out try agian')
-                    self.playerturn = True
                 else:
                     self.player_two_right += self.player_one_right
                     if self.player_two_right >= 5:
-                        self.player_two_right = self.player_two_right % 5
-                        self.playerturn = False
-                    else: 
-                        self.playerturn = False
+                        self.player_two_right = 0
         else:
             if to_left_hand:
                 #Player 2's left hand taps Player 1's left hand
-                if self.player_one_left == 0: 
+                if self.player_one_left == 0 or self.player_two_left == 0: 
                     print('That hand is out try agian')
-                    self.playerturn = False
+                    return False
                 else:
                     self.player_one_left += self.player_two_left
                     if self.player_one_left >= 5:
-                        self.player_one_left = self.player_one_left % 5
-                        self.playerturn = True
-                    else: 
-                        self.playerturn = True
+                        self.player_one_left = 0
             else:
                 #Player 2's right hand taps Player 1's right hand
-                if self.player_one_right == 0: 
+                if self.player_one_right == 0 or self.player_two_right == 0: 
                     print('That hand is out try agian')
-                    self.playerturn = False
+                    return False
                 else:
                     self.player_one_right += self.player_two_right
                     if self.player_one_right >= 5:
-                        self.player_one_right = self.player_one_right % 5
-                        self.playerturn = True
-                    else: 
-                        self.playerturn = True
+                        self.player_one_right = 0
+
         print("\n")
         print(self.player_two_left,"        ",self.player_two_right)
         print(self.player_one_left,"        ",self.player_one_right)
         print("\n")
+        self.playerturn = not self.playerturn
+        return True
+
+class AI:
+    def make_move(self):
+        node = ai.score_moves(game)
+        print(node)
+        for move in node:
+            #print("Move: ", move['move'])
+            #print(move['move'], move['score'])
+            if  move['score'] > move['score']:
+                print(move['move'])
+            else:
+                print(move['move'])
+
+    #     node = self.score_moves(game)
+    #     for move in node:
+    #         if move['score'][node] > move['score']:
+    #             node = move
+    #     print(move['move'], move['score'])
+    #     print (node)
+            
+    def score_moves(self, real_game):
+        state = real_game.get_state()
+        fake_game = Chop_Sticks()
+        fake_game.set_state(state)
+        root_node = {'children': [], 'score': 0}
+        self.recurse_bfs(fake_game, root_node, RECURSION_DEPTH)
+        return root_node['children']
 
 
-game = Chop_Sticks() 
-while game.not_at_end:
-    if not game.player_defeat():
-        hand = input("Left hand = 1, Right hand = 0: ")
-        if hand == 1:
-            game.tap(True)
-        elif hand == 0:
-            game.tap(False)
-        elif hand == 6:
-            game.swap()
-        elif hand == 5:
-            game.split()
+    def recurse_bfs(self, game, node, depth):
+        depth -= 1
+        if depth == 0:
+            return 0
+        if game.ended():
+            if game.player_defeat():
+                return -1
+            else: 
+                return 1
         else:
-            print("try again")
+            print('LL')
+            state = game.get_state()
+            if game.tap(True):
+                node['children'].append({'children': [], 'score': 0, 'move': 'LL'})
+                node['score'] += self.recurse_bfs(game, node['children'][-1], depth)
+                game.set_state(state)
+
+            print('RR')
+            state = game.get_state()
+            if game.tap(False):
+                node['children'].append({'children': [], 'score': 0, 'move': 'RR'})
+                node['score'] += self.recurse_bfs(game, node['children'][-1], depth)
+                game.set_state(state)
+
+            print('LR')
+            state = game.get_state()
+            game.swap()
+            if game.tap(True):
+                node['children'].append({'children': [], 'score': 0, 'move': 'LR'})
+                game.swap()
+                node['score'] += self.recurse_bfs(game, node['children'][-1], depth)
+                game.set_state(state)
+            else:
+                game.swap()
+
+            print('RL')
+            state = game.get_state()
+            game.swap()
+            if game.tap(False):
+                node['children'].append({'children': [], 'score': 0, 'move': 'RL'})
+                game.swap()
+                node['score'] += self.recurse_bfs(game, node['children'][-1], depth)
+                game.set_state(state)
+            else:
+                game.swap()
+
+            print('split')
+            state = game.get_state()
+            if game.split():
+                node['children'].append({'children': [], 'score': 0, 'move': 'split'})
+                node['score'] += self.recurse_bfs(game, node['children'][-1], depth)
+                game.set_state(state)
+
+            return node['score']
+
+game = Chop_Sticks()   
+try:
+    ai = AI()
+    moves = ai.score_moves(game)
+    print(moves)
+    for move in moves:
+        print("Move: ", move['move'])
+ 
+except RuntimeError:
+    print('Stack overflow')
+    exit()
+
+# while not game.player_defeat:
+#     if not game.player_defeat():
+#         if ai.make_move() == 'LL':
+#             game.tap(True)
+#         elif ai.make_move() == 'RR':
+#             game.tap(False)
+#         elif ai.make_move() == 'LR':
+#             game.swap()
+#             game.tap(False)
+#         elif ai.make_move() == 'RL':
+#             game.swap()
+#             game.tap(True)
+#         elif ai.make_move() == 'split':
+#             game.split()
+#         else:
+#             print("try again")
